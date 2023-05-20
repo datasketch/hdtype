197457eae4b2ddfcc35aaf37f8570d4d46c25de7
Jp Marin Diaz <jpmarindiaz@gmail.com>
2023-05-20 17:08:00 -0500

    is_hdvector

---
+++ R/hdvector.R
@@ -0,0 +1,21 @@
+
+
+#' @title Check if x is an hdvector
+#'
+#' @description test for objects of type "hdvector"
+#'
+#' @param x hdtype object to be coerced or tested
+#'
+#' @return returns TRUE or FALSE depending on whether its argument is one of the [available_hdtypes()] or not.
+#'
+#' @examples
+#'
+#' x <- Cat("value1")
+#' is_hdvector(x)
+#'
+#' @export
+is_hdvector <- function(x){
+  av_types <- paste0("hd_",available_hdtypes())
+  hd_class <- class(x)[grepl("hd_", class(x))]
+  hd_class %in% av_types
+}
---
+++ tests/testthat/test-hdvector.R
@@ -0,0 +1,8 @@
+test_that("multiplication works", {
+
+  cat <- Cat("cat")
+  expect_true(is_hdvector(cat))
+
+
+
+})
