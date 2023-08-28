dcb0e39eae1ed7743e86575ff66de80dce9bd785
Jp Marin Diaz <jpmarindiaz@gmail.com>
2023-08-27 22:35:27 -0500

    fixing dependencies

--- R/hdtype-package.R
+++ R/hdtype-package.R
@@ -1,24 +1,8 @@
 #' @keywords internal
 "_PACKAGE"
 
-#' vctrs compatibility functions for math, casting, coercion, and comparison
-#'
-#' These functions are the extensions that allow debkeepr vectors to
-#' work with vctrs.
-#'
-#' @param x,y Vectors.
-#' @param op Arithmetic operation.
-#' @param to Type to cast to.
-#' @param ... Arguments passed on to further methods.
-#' @param .fn Mathematical function.
-#' @param .x Vector.
-#'
-#' @name vctrs-compat
-#'
-NULL
-
 ## usethis namespace: start
 #' @importFrom dstools %||%
-#' @importFrom vctrs vec_cast
+#' @import vctrs
 ## usethis namespace: end
 NULL
